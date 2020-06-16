e4c0488483bd8d204e389ea95b49b11943638c4f
Nariyuki Saito <toshincompos@gmail.com>
2020-06-16 23:47:43 +0900

    Initial commit

---
+++ pkg/apis/growi/group.go
@@ -0,0 +1,6 @@
+// Package growi contains growi API versions.
+//
+// This file ensures Go source parsers acknowledge the growi package
+// and any child packages. It can be removed if any other Go source files are
+// added to this package.
+package growi
